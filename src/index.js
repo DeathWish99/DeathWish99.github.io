d2ab270670eea005577f9216289fe98b12085a89
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2022-09-29 01:44:20 +0700

    three.js test

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+
+const SCENE = THREE.Scene();
+const CANVAS = document.querySelector('#canvas');
+const RENDERER = new THREE.WebGLRenderer({CANVAS});
+
+const FOV = 75;
+const ASPECT = 2;
+const NEAR = 0.1;
+const FAR = 5;
+const CAMERA = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+
+CAMERA.position.z = 2;
+
+let boxWidth = 1;
+let boxHeight = 1;
+let boxDepth = 1;
+let geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+let material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+let cube = new THREE.Mesh(geometry, material);
+
+SCENE.add(cube);
+RENDERER.render(SCENE, CAMERA);
+
