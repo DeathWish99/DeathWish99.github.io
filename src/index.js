6b174b968a659ffff7117a84f7d2203f87ea7100
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2022-09-29 03:17:07 +0700

    IT FINALLY WORKS SOMEHOW

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import * as THREE from 'three';
+const SCENE = new THREE.Scene();
+const CANVAS = document.querySelector('.webgl');
 
-const CANVAS = document.querySelector('#canvas');
-const RENDERER = new THREE.WebGLRenderer({CANVAS});
-
-console.log("we in");
-
-const FOV = 75;
-const ASPECT = 2;
-const NEAR = 0.1;
-const FAR = 5;
-const CAMERA = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
-
-CAMERA.position.z = 2;
+console.log(CANVAS);
+console.log("test")
 
 let boxWidth = 1;
 let boxHeight = 1;
@@ -20,10 +11,27 @@ let geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
 let material = new THREE.MeshBasicMaterial({color: 0x44aa88});
 let cube = new THREE.Mesh(geometry, material);
-const SCENE = new THREE.Scene();
 
 console.log(cube);
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: CANVAS
+})
+renderer.setSize(sizes.width, sizes.height)
+
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 5;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+// SCENE.add(camera);
 
 SCENE.add(cube);
-RENDERER.render(SCENE, CAMERA);
+renderer.render(SCENE, camera);
+console.log(renderer);
 
