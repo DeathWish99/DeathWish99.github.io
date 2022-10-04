a49bb57eb97ee5db861fa8c5b15e20dc02744140
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2022-10-04 16:24:25 +0700

    rotate scene with mouse

--- src/index.js
+++ src/index.js
@@ -1,40 +1,59 @@
+import { addMouseHandler } from './3d/mouseDrag'; './3d/mouseDrag.js';
 import './style/style.css';
 import _ from 'lodash';
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const canvas = document.querySelector('#main-canvas');
+function main(){
 
-let boxWidth = 1;
-let boxHeight = 1;
-let boxDepth = 1;
-let geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const scene = new THREE.Scene();
+    const canvas = document.querySelector('#main-canvas');
+    
+    let boxWidth = 1;
+    let boxHeight = 1;
+    let boxDepth = 1;
+    let geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    
+    let material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    let cube = new THREE.Mesh(geometry, material);
+    
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+    })
+    
+    const fov = 75;
+    const aspect = 2;
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+    
+    scene.add(cube);
+    addMouseHandler(canvas, cube);
 
-let material = new THREE.MeshBasicMaterial({color: 0x44aa88});
-let cube = new THREE.Mesh(geometry, material);
+    function resizeRendererToDisplaySize(renderer){
+        let canvas = renderer.domElement;
+        let width = canvas.clientWidth;
+        let height = canvas.clientHeight;
+        let needResize = canvas.width !== width || canvas.height !== height;
+        if(needResize){
+            renderer.setSize(width, height, false);
+        }
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-
-const fov = 75;
-const aspect = 2;
-const near = 0.1;
-const far = 5;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
-
-scene.add(cube);
-
-function render(time){
-    time *= 0.001;
-    let canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-    cube.rotation.x = time;
-    cube.rotation.y = time;
-
-    renderer.render(scene, camera);
+        return needResize;
+    }
+    
+    function render(){
+        if(resizeRendererToDisplaySize(renderer)){
+            let canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+    
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
     requestAnimationFrame(render);
 }
-requestAnimationFrame(render);
+
+main();
+
